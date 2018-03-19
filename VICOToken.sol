53d17c0a110d9cb705cdb04619a53a03cb70760f
Verified ICOs <34802029+VerifiedICOs@users.noreply.github.com>
2018-03-19 14:19:26 -0600

    Removed useless functions

--- VICOToken.sol
+++ VICOToken.sol
@@ -1,10 +1,5 @@
 pragma solidity ^0.4.19;
 
-contract tokenRecipient { 
-    
-    function receiveApproval(address _from, uint256 _value, address _token, bytes _extraData) public; 
-    }
-
 contract VICOToken {
 
 	string public name = 'VICO Vote Token';
@@ -31,18 +26,4 @@ contract VICOToken {
         Transfer(msg.sender, _to, _value);                   
         return true;
     }
-
-    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
-        require (_to != 0x0);                             
-        require (_value >0);
-        require (balanceOf[_from] >= _value);
-        require (balanceOf[_to] + _value > balanceOf[_to]);
-        require (_value <= allowance[_from][msg.sender]);    
-        balanceOf[_from] -= _value;
-        balanceOf[_to] += _value; 
-        allowance[_from][msg.sender] -= _value;
-        Transfer(_from, _to, _value);
-        return true;
-    }
-
 }
